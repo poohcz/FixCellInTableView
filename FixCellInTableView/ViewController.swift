6f73d5cce603fc968dbe179902725755bc69d0a3
Dan <poohcz@nate.com>
2024-07-05 13:44:26 +0900

    Initial Commit

---
+++ FixCellInTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FixCellInTableView
+//
+//  Created by 김동율 on 7/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
